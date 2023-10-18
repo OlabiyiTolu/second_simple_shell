ef727605dd894231f20798a295cb96735dc75fcd
Tolu <olabiyitolu@gmail.com>
2023-10-18 17:08:33 +0100

    simple shell

--- main.c
+++ main.c
@@ -2,38 +2,42 @@
 
 int main(int argc, char **argv)
 {
-    MyShellInfo info = MY_SHELL_INFO_INIT;
-    int fd = 2;
+  MyShellInfo info = MY_SHELL_INFO_INIT;
+  // Initialize the commandBuffer field
+  info.commandBuffer = malloc(sizeof(char *) * 10);
 
-    asm (
-        "mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r" (fd)
-        : "r" (fd)
-    );
+  int fd = 2;
 
-    if (argc == 2)
+  asm (
+    "mov %1, %0\n\t"
+    "add $3, %0"
+    : "=r" ((int)fd)
+    : "r" (fd)
+  );
+
+  if (argc == 2)
+  {
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
     {
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1)
-        {
-            if (errno == EACCES)
-                my_exit(&info);
-            if (errno == ENOENT)
-            {
-                my_puts(argv[0]);
-                my_puts(": 0: Can't open ");
-                my_puts(argv[1]);
-                my_putchar('\n');
-                my_putchar(MY_BUF_FLUSH);
-                my_exit(&info);
-            }
-            return EXIT_FAILURE;
-        }
-        info.readFd = fd;
+      if (errno == EACCES)
+        my_exit((int)&info);
+      if (errno == ENOENT)
+      {
+        my_puts(argv[0]);
+        my_puts(": 0: Can't open ");
+        my_puts(argv[1]);
+        my_putchar('\n');
+        my_putchar(MY_BUF_FLUSH);
+        my_exit((int)&info);
+      }
+      return EXIT_FAILURE;
     }
-    populate_my_env_list(&info);
-    read_my_history(&info);
-    my_shell(&info, argv);
-    return EXIT_SUCCESS;
+    info.readFd = fd;
+  }
+
+  populate_my_env_list(&info);
+  read_my_history(&info);
+  my_shell(&info, argv);
+  return EXIT_SUCCESS;
 }
