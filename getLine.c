d9b8f7d220b9f565a70bdab11190e5ef56b7bd65
Tolu <olabiyitolu@gmail.com>
2023-10-18 19:49:13 +0100

    simple shell

--- getLine.c
+++ getLine.c
@@ -1,73 +1,170 @@
-#include "shell.h"
+#include "my_shell.h"
 
 /**
- * clear_my_info - Initializes MyShellInfo struct.
- * @info: Pointer to the struct to be cleared.
+ * my_input_buffer - buffers chained commands
+ * @info: MyShellInfo struct
+ * @buffer: address of the buffer
+ * @length: address of length variable
+ *
+ * Return: bytes read
  */
-void clear_my_info(MyShellInfo *info)
+ssize_t my_input_buffer(MyShellInfo *info, char **buffer, size_t *length)
 {
-    info->argument = NULL;
-    info->arguments = NULL;
-    info->path = NULL;
-    info->argumentCount = 0;
-}
+    ssize_t read_bytes = 0;
+    size_t length_p = 0;
 
-/**
- * set_my_info - Initializes MyShellInfo struct.
- * @info: Pointer to the struct to be initialized.
- * @av: Argument vector.
- */
-void set_my_info(MyShellInfo *info, char **av)
-{
-    int i = 0;
-
-    info->fileName = av[0];
-    if (info->argument)
+    if (!*length) /* if nothing left in the buffer, fill it */
     {
-        info->arguments = my_strtow(info->argument, " \t");
-        if (!info->arguments)
+        /* my_bfree((void **)info->commandBuffer); */
+        free(*buffer);
+        *buffer = NULL;
+        signal(SIGINT, sigint_handler);
+#if MY_USE_GETLINE
+        read_bytes = my_getline(buffer, &length_p, stdin);
+#else
+        read_bytes = my_getline_alternative(info, buffer, &length_p);
+#endif
+        if (read_bytes > 0)
         {
-            info->arguments = (char **)malloc(sizeof(char *) * 2);
-            if (info->arguments)
+            if ((*buffer)[read_bytes - 1] == '\n')
             {
-                info->arguments[0] = my_strdup(info->argument);
-                info->arguments[1] = NULL;
+                (*buffer)[read_bytes - 1] = '\0'; /* remove trailing newline */
+                read_bytes--;
+            }
+            info->lineCountFlag = 1;
+            remove_comments(*buffer);
+            build_my_history_list(info, *buffer, info->historyCount++);
+            /* if (my_strchr(*buffer, ';')) is this a command chain? */
+            {
+                *length = read_bytes;
+                info->commandBuffer = buffer;
             }
         }
-        for (i = 0; info->arguments && info->arguments[i]; i++)
-            ;
-        info->argumentCount = i;
-
-        replace_my_alias(info);
-        replace_my_vars(info);
     }
+    return read_bytes;
 }
 
 /**
- * free_my_info - Frees MyShellInfo struct fields.
- * @info: Pointer to the struct.
- * @all: True if freeing all fields.
+ * my_get_input - gets a line minus the newline
+ * @info: MyShellInfo struct
+ *
+ * Return: bytes read
  */
-void free_my_info(MyShellInfo *info, int all)
+ssize_t my_get_input(MyShellInfo *info)
 {
-    my_ffree(info->arguments);
-    info->arguments = NULL;
-    info->path = NULL;
-    if (all)
+    static char *buffer; /* the ';' command chain buffer */
+    static size_t i, j, length;
+    ssize_t read_bytes = 0;
+    char **buffer_p = &(info->argument), *p;
+
+    my_putchar(MY_BUF_FLUSH);
+    read_bytes = my_input_buffer(info, &buffer, &length);
+    if (read_bytes == -1) /* EOF */
+        return -1;
+    if (length) /* we have commands left in the chain buffer */
     {
-        if (!info->commandBuffer)
-            free(info->argument);
-        if (info->environment)
-            free_my_list(&(info->environment));
-        if (info->history)
-            free_my_list(&(info->history));
-        if (info->alias)
-            free_my_list(&(info->alias));
-        my_ffree(info->my_environ);
-        info->my_environ = NULL;
-        my_bfree((void **)info->commandBuffer);
-        if (info->readFd > 2)
-            close(info->readFd);
-        my_putchar(MY_BUF_FLUSH);
+        j = i;       /* init new iterator to current buffer position */
+        p = buffer + i; /* get pointer for return */
+
+        my_check_chain(info, buffer, &j, i, length);
+        while (j < length) /* iterate to semicolon or end */
+        {
+            if (my_is_chain(info, buffer, &j))
+                break;
+            j++;
+        }
+
+        i = j + 1;     /* increment past nulled ';'' */
+        if (i >= length) /* reached end of buffer? */
+        {
+            i = length = 0; /* reset position and length */
+            info->commandBufferType = MY_CMD_NORM;
+        }
+
+        *buffer_p = p; /* pass back pointer to current command position */
+        return my_strlen(p); /* return length of current command */
     }
+
+    *buffer_p = buffer; /* else not a chain, pass back buffer from my_getline() */
+    return read_bytes;   /* return length of buffer from my_getline() */
+}
+
+/**
+ * my_read_buffer - reads a buffer
+ * @info: MyShellInfo struct
+ * @buffer: buffer
+ * @i: size
+ *
+ * Return: read_bytes
+ */
+ssize_t my_read_buffer(MyShellInfo *info, char *buffer, size_t *i)
+{
+    ssize_t read_bytes = 0;
+
+    if (*i)
+        return 0;
+    read_bytes = read(info->readFd, buffer, MY_READ_BUF_SIZE);
+    if (read_bytes >= 0)
+        *i = (size_t)read_bytes;
+    return read_bytes;
+}
+
+/**
+ * my_getline_alternative - gets the next line of input from STDIN
+ * @info: MyShellInfo struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
+ *
+ * Return: size
+ */
+int my_getline_alternative(MyShellInfo *info, char **ptr, size_t *length)
+{
+    static char buffer[MY_READ_BUF_SIZE];
+    static size_t i, len;
+    size_t k;
+    ssize_t read_bytes = 0;
+    char *p = NULL, *new_p = NULL, *c;
+
+    p = *ptr;
+    if (p && length)
+        k = *length;
+    if (i == len)
+        i = len = 0;
+
+    read_bytes = my_read_buffer(info, buffer, &len);
+    if (read_bytes == -1 || (read_bytes == 0 && len == 0))
+        return -1;
+
+    c = my_strchr(buffer + i, '\n');
+    k = c ? 1 + (size_t)(c - buffer) : len;
+    new_p = my_realloc(p, k, p ? k : k + 1);
+    if (!new_p) /* MALLOC FAILURE! */
+        return p ? (my_ffree((void **)p), -1) : -1;
+
+    if (p)
+        my_strncat(new_p, buffer + i, k - i);
+    else
+        my_strncpy(new_p, buffer + i, k - i + 1);
+
+    k -= i;
+    i = k;
+    p = new_p;
+
+    if (length)
+        *length = k;
+    *ptr = p;
+    return k;
+}
+
+/**
+ * my_sigint_handler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void my_sigint_handler(__attribute__((unused)) int sig_num)
+{
+    my_puts("\n");
+    my_puts("$ ");
+    my_putchar(MY_BUF_FLUSH);
 }
