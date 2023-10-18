3a3d4f2854f0353806cba4c896a9c7606cf7afb4
Tolu <olabiyitolu@gmail.com>
2023-10-18 17:31:54 +0100

    simple shell

--- main.c
+++ main.c
@@ -2,42 +2,43 @@
 
 int main(int argc, char **argv)
 {
-  MyShellInfo info = { .commandBuffer = NULL };
+    MyShellInfo info;
+    info.commandBuffer = NULL;
 
-  int fd = 2;
+    int fd = 2;
 
-  asm (
-    "mov %1, %0\n\t"
-    "add $3, %0"
-    : "=r" ((int)fd)
-    : "r" (fd)
-  );
+    asm (
+        "mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r" ((int)fd)
+        : "r" (fd)
+    );
 
-  if (argc == 2)
-  {
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
+    if (argc == 2)
     {
-      if (errno == EACCES)
-        my_exit(&info);
-      if (errno == ENOENT)
-      {
-        my_puts(argv[0]);
-        my_puts(": 0: Can't open ");
-        my_puts(argv[1]);
-        my_putchar('\n');
-        my_putchar(MY_BUF_FLUSH);
-        my_exit(&info);
-      }
-      return EXIT_FAILURE;
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+        {
+            if (errno == EACCES)
+                my_exit(&info);
+            if (errno == ENOENT)
+            {
+                my_puts(argv[0]);
+                my_puts(": 0: Can't open ");
+                my_puts(argv[1]);
+                my_putchar('\n');
+                my_putchar(MY_BUF_FLUSH);
+                my_exit(&info);
+            }
+            return EXIT_FAILURE;
+        }
+        info.readFd = fd;
     }
-    info.readFd = fd;
-  }
 
-  info.commandBuffer = malloc(sizeof(char *) * 10);
+    info.commandBuffer = malloc(sizeof(char *) * 10);
 
-  populate_my_env_list(&info);
-  read_my_history(&info);
-  my_shell(&info, argv);
-  return EXIT_SUCCESS;
+    populate_my_env_list(&info);
+    read_my_history(&info);
+    my_shell(&info, argv);
+    return EXIT_SUCCESS;
 }
